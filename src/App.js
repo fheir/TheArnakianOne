b7c498c2d762c42bd0131b5f4c1376cb8e8db994
Anton Tsai <anton.ctsai@gmail.com>
2021-01-14 14:26:38 -0500

    Add discard pile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App(props) {
   return (
     <div>
       <h1>Hello World! {props.name}</h1>
-      <DrawPile/>
+      <div className="float-container">
+        <DrawPile count={10} name="draw-pile"/>
+        <DrawPile count={0} name="discard-pile"/>
+      </div>
     </div>
     
   );
@@ -16,12 +19,23 @@ function App(props) {
 class DrawPile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {cardCount: 10};
+    this.state = {cardCount: props.count}; 
+
+    this.drawCard = this.drawCard.bind(this);
   }
+
+  drawCard() {
+    this.setState({
+      cardCount: this.state.cardCount - 1
+    });
+  }
+
   render() {
     return (
-      <div className="DrawPile">
+      <div className={this.props.name}>
         <img className="Draw" src={logo}/>
+        <h1>{this.state.cardCount}</h1>
+        <button onClick={this.drawCard}>Draw</button>
       </div>
     );
   }
